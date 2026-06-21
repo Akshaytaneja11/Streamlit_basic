86d813c89a922e192531cafe86b607121e4e58b9
Akshay Taneja <taneja90063@gmail.com>
2026-06-21 16:21:30 +0000

    Streamlit basics and Summarizer

---
+++ streamlit_demo.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Streamlit Demo",
+    layout="centered"
+)
+
+st.title("welcome to Streamlit")
+
+st.header("A simple and powerful way to build web apps in Python")
+
+st.subheader("Perfect for data science, ML, and teaching")
+
